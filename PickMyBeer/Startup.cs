be3921abb0f4489c0abd63454e71c35016973456
NLHawkins <nathanlhawkins@gmail.com>
2017-03-09 14:56:53 -0500

    Add project files.

---
+++ PickMyBeer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PickMyBeer.Startup))]
+namespace PickMyBeer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
